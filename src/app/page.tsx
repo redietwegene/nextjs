59f10c67b7535b84349c974edb1b18f6b2b92444
Rediet Haileab <wegenerediet@gmail.com>
2024-10-11 17:00:01 +0300

    FIRST MODFICATION

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 // import Image from "next/image";
+import React from 'react'
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main>
       <h1>hello next</h1>
+      <Link href="/users"> userspage</Link>
    </main>
   );
 }
-import React from 'react'
+
+
 
 
---
+++ src/app/users/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Userspage = () => {
+  return (
+      <div>
+          hello dear users
+      
+    </div>
+  )
+}
+
+export default Userspage
